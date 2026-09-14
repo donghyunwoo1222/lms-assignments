d2430590aa3475d927fae303fb53844cd3a3f8e7
donghyunwoo1222 <324273907+donghyunwoo1222@users.noreply.github.com>
2026-09-14 16:37:56 +0900

    [chapter24 개인과제] 제출 · 2026-09-09 09:41

---
+++ 테스트/55-chapter24-개인과제/first_print.py
@@ -0,0 +1,19 @@
+print("Hello, Python!") #문자 str
+
+print(100+200) #정수 int
+print("100+200") #문자 str
+
+print("이름:","길동")
+print("학습 시간:",2)
+#따옴표 사이에는 띄어쓰기가 자동으로 들어간다.
+
+name = "우동현"
+study_time = 2
+print(name, "님이 학습한 시간은", study_time, "시간입니다.")
+
+from datetime import date
+print("이름 :","우동현")
+print("학습일",date.today())
+print("좋아하는 숫자",7)
+print("목표","업무 자동화 프로그램 만들기")
+
